043be902095b5b3b45b3d26536b164b9e01b9afc
dokasmn <novocsm@gmail.com>
2024-11-21 16:54:00 -0300

    :tada: Feat: iniciando projeto

---
+++ src/main/java/com/example/dz7Api/Dz7ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dz7Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz7ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dz7ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dz7Api/Dz7ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz7Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz7ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
