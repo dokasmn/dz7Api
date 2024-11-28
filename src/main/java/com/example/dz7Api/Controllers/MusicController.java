4ab8545ee79fc4dc163603e9271648f7e5e28aee
dokasmn <novocsm@gmail.com>
2024-11-28 17:10:49 -0300

    🙌 Feat: adicionando endpoint de listar musicas

--- src/main/java/com/example/dz7Api/Controllers/MusicController.java
+++ src/main/java/com/example/dz7Api/Controllers/MusicController.java
@@ -3,6 +3,15 @@ package com.example.dz7Api.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dz7Api.Models.Music;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 
 
@@ -10,9 +19,14 @@ import org.springframework.ui.Model;
 @RequestMapping("/api/music")
 public class MusicController {
 
+    List<Music> musics = new ArrayList<>();
+
     @GetMapping
-    public String Index(Model model){
-        return "music";
+    public ResponseEntity<List<Music>> listMusics (Model model){
+        if (musics.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(musics, HttpStatus.OK);
     }
 
 }
