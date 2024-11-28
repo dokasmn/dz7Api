283777d15edefa51d91ccab8907f9ec8e87f3e6f
dokasmn <novocsm@gmail.com>
2024-11-28 14:55:39 -0300

    :wrench: Config: adicionando dependencias e construindo pastas da aplicação

--- src/main/java/com/example/dz7Api/Controllers/MusicController.java
+++ src/main/java/com/example/dz7Api/Controllers/MusicController.java
@@ -1,5 +1,15 @@
 package com.example.dz7Api.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class MusicController {
-    
+
+    @GetMapping("/music")
+    public String Index(Model model){
+        return "helloPage";
+    }
+
 }
