e152f60777052849b085e40aee9eb80d5ec7c9a0
Moreira Cauan (SO/BUD3-ETS) <novocsm@gmail.com>
2024-11-29 11:50:40 -0300

    :sparkles: Feat: adicionando post de music e get por id, repository e services

--- src/main/java/com/example/dz7Api/Controllers/MusicController.java
+++ src/main/java/com/example/dz7Api/Controllers/MusicController.java
@@ -1,13 +1,12 @@
 package com.example.dz7Api.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.dz7Api.Models.Music;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 
@@ -21,12 +20,33 @@ public class MusicController {
 
     List<Music> musics = new ArrayList<>();
 
+
     @GetMapping
-    public ResponseEntity<List<Music>> listMusics (Model model){
-        if (musics.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(musics, HttpStatus.OK);
+    public ResponseEntity<List<Music>> listMusics (Model model) {
+        if (musics.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(musics);
     }
 
+
+    @GetMapping("{musicName}")
+    public ResponseEntity<Music> getMusic (@PathVariable String musicName) {
+
+        if (musicName == null || musicName.isEmpty())
+            return ResponseEntity.badRequest().build();
+
+        Optional<Music> foundMusic = musics.stream()
+                                            .filter(music -> music.getMusicName().equalsIgnoreCase(musicName))
+                                            .findFirst();
+        if (foundMusic.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(foundMusic.get());
+    }
+
+
+   @PostMapping("")
+   public ResponseEntity<String> addMusic (@RequestBody Music newMusic) {
+       return ResponseEntity.created()
+   }
 }
--- src/main/java/com/example/dz7Api/Models/Music.java
+++ src/main/java/com/example/dz7Api/Models/Music.java
@@ -1,38 +1,33 @@
 package com.example.dz7Api.Models;
 
-import java.sql.Time;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
-import nonapi.io.github.classgraph.json.Id;
 
+import java.sql.Time;
+
+
+@Getter
+@Setter
 @Entity
 public class Music {
-    @Getter
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idMusic;
-
-    @Getter
-    @Setter
     private String musicName;
-
-    @Getter
-    @Setter
     private String musicLink;
 
-    @Getter
-    @Setter
-    private Time musicDurationTime;
-
-    @Getter
-    @Setter
+    // para duração é possível utilizar o Duration, que é mais preciso e flexível, conferir possibilidade
+    private Time musicDuration;
     private String musicGenre;
 
-    public Music(String musicName, String musicLink, Time musicDurationTime, String musicGenre) {
+
+    public Music(String musicName, String musicLink, Time musicDuration, String musicGenre) {
         if (musicName == null || musicName.isEmpty()) {
             throw new IllegalArgumentException("A música deve ter um nome!");
         }
@@ -41,18 +36,21 @@ public class Music {
             throw new IllegalArgumentException("A música deve ter um link!");
         }
 
-        if (musicDurationTime == null) {
+        if (musicDuration == null) {
             throw new IllegalArgumentException("A música deve ter um tempo de duração!");
         }
 
         if (musicGenre == null || musicGenre.isEmpty()) {
             throw new IllegalArgumentException("A música deve ter um gênero!");
         }
-        
+
         this.musicName = musicName;
         this.musicLink = musicLink;
-        this.musicDurationTime = musicDurationTime;
+        this.musicDuration = musicDuration;
         this.musicGenre = musicGenre;
     }
-    
+
+    // Pra gerar um construtor sem parametros pode-se usar o Lombok, conferir necessidade
+    // @NoArgsConstructor
 }
+
