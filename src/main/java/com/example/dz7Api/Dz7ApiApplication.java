47db79599ace36b873175c0f6d4b2c4a30b88f43
dokasmn <novocsm@gmail.com>
2024-12-03 11:04:40 -0300

    :bugffix: resolvendo bug de conexão com banco de dados

--- src/main/java/com/example/dz7Api/Dz7ApiApplication.java
+++ src/main/java/com/example/dz7Api/Dz7ApiApplication.java
@@ -3,7 +3,7 @@ package com.example.dz7Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.dz7Api")
 public class Dz7ApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/dz7Api/Repositories/MusicRepository.java
+++ src/main/java/com/example/dz7Api/Repositories/MusicRepository.java
@@ -2,7 +2,9 @@ package com.example.dz7Api.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.example.dz7Api.Models.Music;
+import com.example.dz7Api.Models.Music;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {}
