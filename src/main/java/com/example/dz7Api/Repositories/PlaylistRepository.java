92881f4e5dedc7afd043104c1a93f8603815c220
Moreira Cauan (SO/BUD3-ETS) <novocsm@gmail.com>
2024-12-05 08:28:04 -0300

    :construction: WIP: atualizando endpoints de playlist com a lógica de usuário

--- src/main/java/com/example/dz7Api/Repositories/PlaylistRepository.java
+++ src/main/java/com/example/dz7Api/Repositories/PlaylistRepository.java
@@ -1,9 +1,16 @@
 package com.example.dz7Api.Repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.dz7Api.Models.Playlist;
 
 @Repository
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {}
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findAllByUserId(Long userId);
+
+    Optional<Playlist> findByIdAndUserId(Long playlistId, Long userId);
+}
