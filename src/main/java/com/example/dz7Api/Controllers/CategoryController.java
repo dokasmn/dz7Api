b70791f7133cd254a0293f7e2fa262db09c03bf0
dokasmn <novocsm@gmail.com>
2024-12-05 20:49:31 -0300

    :sparkles: Feat: entregando endpoints de category

--- src/main/java/com/example/dz7Api/Controllers/CategoryController.java
+++ src/main/java/com/example/dz7Api/Controllers/CategoryController.java
@@ -1,10 +1,18 @@
 package com.example.dz7Api.Controllers;
 
 import com.example.dz7Api.Models.Category;
+import com.example.dz7Api.Models.Music;
 import com.example.dz7Api.Models.base.BaseUser;
 import com.example.dz7Api.Repositories.UserRepository;
 import com.example.dz7Api.Services.CategoryService;
+import com.example.dz7Api.Services.MusicService;
+import com.example.dz7Api.dto.CategoryDTO;
+import com.example.dz7Api.dto.CategoryInputDTO;
+import com.example.dz7Api.dto.CategoryUpdateDTO;
+
 import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,29 +24,34 @@ public class CategoryController {
 
     private final CategoryService categoryService;
     private final UserRepository userRepository;
+    private final MusicService musicService;
 
 
-    public CategoryController(CategoryService categoryService, UserRepository userRepository) {
+    public CategoryController(CategoryService categoryService, UserRepository userRepository, MusicService musicService) {
         this.categoryService = categoryService;
         this.userRepository = userRepository;
+        this.musicService = musicService;
     }
 
 
     @GetMapping
-    public ResponseEntity<List<Category>> listCategories() {
+    public ResponseEntity<List<CategoryDTO>> listCategories() {
         List<Category> categories = categoryService.findAll();
         if (categories.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(categories);
+        List<CategoryDTO> categoryDTOs = categories.stream()
+            .map(categoryService::toCategoryDTO)
+            .toList();
+        return ResponseEntity.ok(categoryDTOs);
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
         try {
             Category category = categoryService.getCategoryById(id);
-            return ResponseEntity.ok(category);
+            return ResponseEntity.ok(categoryService.toCategoryDTO(category));
         } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
@@ -46,7 +59,10 @@ public class CategoryController {
 
 
     @PostMapping
-    public ResponseEntity<Category> saveCategory(@RequestBody Category category, @RequestParam Long userId, @RequestParam String userPassword) {
+    public ResponseEntity<CategoryDTO> saveCategory(
+        @RequestBody CategoryInputDTO categoryInputDTO,
+        @RequestParam Long userId,
+        @RequestParam String userPassword) {
         try {
             BaseUser user = userRepository.findById(userId)
                     .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
@@ -58,16 +74,30 @@ public class CategoryController {
                 return ResponseEntity.status(403).build();
             }
 
+            Category category = new Category();
+            category.setCategoryName(categoryInputDTO.getCategoryName());
+            category.setMinCategoryTemperature(categoryInputDTO.getMinCategoryTemperature());
+            category.setMaxCategoryTemperature(categoryInputDTO.getMaxCategoryTemperature());
+
+            List<Music> musics = categoryInputDTO.getMusicIds().stream()
+                    .map(musicService::getMusicById)
+                    .toList();
+            category.setMusics(musics);
             Category savedCategory = categoryService.saveCategory(category);
-            return ResponseEntity.status(201).body(savedCategory);
+            CategoryDTO categoryDTO = categoryService.toCategoryDTO(savedCategory);
+            return ResponseEntity.status(201).body(categoryDTO);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.badRequest().build();
         }
     }
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category, @RequestParam Long userId, @RequestParam String userPassword) {
+    public ResponseEntity<CategoryDTO> updateCategory(
+        @PathVariable Long id,
+        @RequestBody CategoryUpdateDTO categoryUpdateDTO,
+        @RequestParam Long userId,
+        @RequestParam String userPassword) {
         try {
             BaseUser user = userRepository.findById(userId)
                     .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
@@ -79,10 +109,17 @@ public class CategoryController {
                 return ResponseEntity.status(403).build();
             }
 
-            Category updatedCategory = categoryService.updateCategory(id, category);
-            return ResponseEntity.ok(updatedCategory);
+            Category existingCategory = categoryService.getCategoryById(id);
+            existingCategory.setCategoryName(categoryUpdateDTO.getCategoryName());
+            existingCategory.setMinCategoryTemperature(categoryUpdateDTO.getMinCategoryTemperature());
+            existingCategory.setMaxCategoryTemperature(categoryUpdateDTO.getMaxCategoryTemperature());
+            Category updatedCategory = categoryService.updateCategory(id, existingCategory);
+            CategoryDTO categoryDTO = categoryService.toCategoryDTO(updatedCategory);
+            return ResponseEntity.ok(categoryDTO);
         } catch (EntityNotFoundException e) {
             return ResponseEntity.notFound().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 
@@ -106,4 +143,71 @@ public class CategoryController {
             return ResponseEntity.notFound().build();
         }
     }
+
+
+    @PostMapping("/{categoryId}/add-music/{musicId}")
+    public ResponseEntity<Void> addMusicToCategory(@PathVariable Long categoryId,
+                                                @PathVariable Long musicId,
+                                                @RequestParam Long userId,
+                                                @RequestParam String userPassword) {
+        BaseUser user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+
+        if (!user.getUserPassword().equals(userPassword)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        try {
+            Music music = musicService.getMusicById(musicId);
+            Category category = categoryService.getCategoryById(categoryId);
+
+            boolean isOwner = music.getOwners().stream()
+                .anyMatch(owner -> owner.getId().equals(user.getId()));
+            boolean isAdmin = "Admin".equals(user.getRole());
+
+            if (isOwner || isAdmin) {
+                categoryService.addMusicToCategory(categoryId, musicId);
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{categoryId}/remove-music/{musicId}")
+    public ResponseEntity<Void> removeMusicFromCategory(
+        @PathVariable Long categoryId,
+        @PathVariable Long musicId,
+        @RequestParam Long userId,
+        @RequestParam String userPassword
+    ) {
+        BaseUser user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+
+        if (!user.getUserPassword().equals(userPassword)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        try {
+            Music music = musicService.getMusicById(musicId);
+            Category category = categoryService.getCategoryById(categoryId);
+            boolean isOwner = music.getOwners().stream()
+                .anyMatch(owner -> owner.getId().equals(user.getId()));
+            boolean isAdmin = "Admin".equals(user.getRole());
+
+            if (isOwner || isAdmin) {
+                categoryService.removeMusicFromCategory(categoryId, musicId);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- src/main/java/com/example/dz7Api/dto/MusicCategoryDTO.java
+++ src/main/java/com/example/dz7Api/dto/MusicCategoryDTO.java
@@ -1,81 +1,96 @@
 package com.example.dz7Api.dto;
 
-import java.util.List;
-
-public class MusicDTO {
-
+public class MusicCategoryDTO {
     private Long idMusic;
     private String musicName;
     private String musicLink;
     private String musicDuration;
     private String musicGenre;
     private String musicCountry;
-    private List<OwnerDTO> owners;
+    private String category;
+
+
+    public MusicCategoryDTO(){}
 
     
-    public MusicDTO(Long idMusic, String musicName, String musicLink, String musicDuration, String musicGenre, String musicCountry, List<OwnerDTO> owners) {
+    public MusicCategoryDTO(Long idMusic, String musicName, String musicLink, String musicDuration, String musicGenre,
+            String musicCountry, String category) {
         this.idMusic = idMusic;
         this.musicName = musicName;
         this.musicLink = musicLink;
         this.musicDuration = musicDuration;
         this.musicGenre = musicGenre;
         this.musicCountry = musicCountry;
-        this.owners = owners;
+        this.category = category;
     }
+
+
     public Long getIdMusic() {
         return idMusic;
     }
+
 
     public void setIdMusic(Long idMusic) {
         this.idMusic = idMusic;
     }
 
+
     public String getMusicName() {
         return musicName;
     }
+
 
     public void setMusicName(String musicName) {
         this.musicName = musicName;
     }
 
+
     public String getMusicLink() {
         return musicLink;
     }
+
 
     public void setMusicLink(String musicLink) {
         this.musicLink = musicLink;
     }
 
+
     public String getMusicDuration() {
         return musicDuration;
     }
+
 
     public void setMusicDuration(String musicDuration) {
         this.musicDuration = musicDuration;
     }
 
+
     public String getMusicGenre() {
         return musicGenre;
     }
+
 
     public void setMusicGenre(String musicGenre) {
         this.musicGenre = musicGenre;
     }
 
+
     public String getMusicCountry() {
         return musicCountry;
     }
+
 
     public void setMusicCountry(String musicCountry) {
         this.musicCountry = musicCountry;
     }
 
-    public List<OwnerDTO> getOwners() {
-        return owners;
+
+    public String getCategory() {
+        return category;
     }
 
-    public void setOwners(List<OwnerDTO> owners) {
-        this.owners = owners;
-    }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
